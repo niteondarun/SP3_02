fe69c7366d5c463afa9043fd218eeb1877a79a7b
niteondarun <61751915+niteondarun@users.noreply.github.com>
2020-03-09 16:12:53 -0700

    Added class Shape.cs edited Circle.cs to inherit from Shape.cs

---
+++ Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP3_02
+{
+    class Shape
+    {
+        public Shape(int size, int rColor, int gColor, int bColor)
+        {
+            SizeOfShape = size;
+            RedColor = rColor;
+            GreenColor = gColor;
+            BlueColor = bColor;
+        }
+
+        public int SizeOfShape { get; set; }
+        public int RedColor { get; set; }
+        public int GreenColor { get; set; }
+        public int BlueColor { get; set; }
+    }
+}
