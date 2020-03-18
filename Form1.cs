1ec842a122513737cadfc72c891760a68a6b109a
niteondarun <61751915+niteondarun@users.noreply.github.com>
2020-03-17 22:51:28 -0400

    Team Collaboration

--- Form1.cs
+++ Form1.cs
@@ -15,12 +15,14 @@ namespace SP3_02
         public Form1()
         {
             InitializeComponent();
-            
-            
         }
 
         ColorDialog ColorSelect = new ColorDialog();
+        ColorDialog BackgroundColorSelect = new ColorDialog();
+        Pen drawPen = new Pen(Color.Red);
+        new Color RGB;
         
+        int shape = 0;
 
         int RedColor, BlueColor, GreenColor;
 
@@ -29,7 +31,7 @@ namespace SP3_02
             if (ColorSelect.ShowDialog() == DialogResult.OK)
             {
                 btn_PaintColor_Click.BackColor = ColorSelect.Color;
-                Color RGB = btn_BackgroundColor_Click.BackColor;
+                RGB = ColorSelect.Color;
                 RedColor = RGB.R;
                 BlueColor = RGB.B;
                 GreenColor = RGB.G;
@@ -39,40 +41,49 @@ namespace SP3_02
 
         private void btn_Square_Click_Click(object sender, EventArgs e)
         {
-            //panel2.Invalidate();
+
         }
 
         private void btn_Square_Click_Paint(object sender, PaintEventArgs e)
         {
-            panel2.Invalidate();
+
         }
 
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Pen drawPen = new Pen(Color.FromArgb(RedColor, BlueColor, GreenColor));
+            drawPen.Color = RGB;
             drawPen.Width = 8;
             int size = int.Parse(TxtBox_SizeInput_Type.Text);
             int x = int.Parse(textBox1.Text);
             int y = int.Parse(textBox2.Text);
-            g.DrawRectangle(drawPen, x, y, size, size);
+            if (shape == 1)
+            {
+                g.DrawRectangle(drawPen, x, y, size, size);
+            }
+
         }
 
         private void btn_BackgroundColor_Click_Click(object sender, EventArgs e)
         {
-            if (ColorSelect.ShowDialog() == DialogResult.OK)
+            if (BackgroundColorSelect.ShowDialog() == DialogResult.OK)
             {
-                panel2.BackColor = ColorSelect.Color;
-                btn_BackgroundColor_Click.BackColor = ColorSelect.Color;
+                panel2.BackColor = BackgroundColorSelect.Color;
+                btn_BackgroundColor_Click.BackColor = BackgroundColorSelect.Color;
             }
         }
 
+
+        /// ////////////////////////////////////////////////////////////////////////////////////////
         //added trying to figure stuff out, have to keep it. probably wont use it
         private void btn_Rectangle_Click_Paint(object sender, PaintEventArgs e)
         {
-            
+            shape = 1;      //Tells paint to draw a rectangle
+            int size = int.Parse(TxtBox_SizeInput_Type.Text);
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            Rectangle z = new Rectangle(x, y, size, size);
+            panel2.Invalidate(z);
         }
-
-        
     }
 }
